d1889e75104ab430575c1de5efce3af028a1416b
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-18 02:08:49 +0000

    1-helo

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 """
 Start Flask application.
+
+This script initializes a Flask application and defines two routes:
+- '/' returns 'Hello HBNB!'
+- '/hbnb' returns 'HBNB'
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
-    """Returns 'Hello HBNB!'"""
+    """Return 'Hello HBNB!'."""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns 'HBNB'"""
+    """Return 'HBNB'."""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
